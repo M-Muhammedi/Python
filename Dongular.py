ab6f065eaf55d83a5614fd9a9e944627f28a18ef
Mustafa Muhammedi <146854084+M-Muhammedi@users.noreply.github.com>
2024-08-04 16:43:23 +0300

    Add files via upload

---
+++ Dongular.py
@@ -0,0 +1,12 @@
+
+meyveler = ["elma", "muz", "çilek", "nar"]
+
+    # for döngüsü
+for i in meyveler:
+    print(i)
+
+        # while döngüsü
+count = 0
+while count < 5:
+    print(count)
+    count += 1
