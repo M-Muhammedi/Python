63b9ab80bfc39acf76cc6e96b0e1298e5f710252
Mustafa Muhammedi <146854084+M-Muhammedi@users.noreply.github.com>
2024-08-04 18:11:25 +0300

    Add files via upload

---
+++ Fonksiyon.py
@@ -0,0 +1,29 @@
+
+
+# Kişisel bilgiler
+def bilgi(isim:str,soyisim:str,yas:int):
+    return print(f"Adınız : {isim} \nSoyAdınız : {soyisim} \nYaşınız : {yas}")
+
+
+kisi1 = bilgi("Mustafa","Muhammedi",2000)
+print(kisi1)
+
+#       Şuanki Yıl
+def yil():
+    import datetime
+    return datetime.datetime.now().year
+
+
+#  Ysş hesaplama Yıl Fonksiyonu kullanarak
+def YasHesaplama(dogumYil):
+    return yil()-dogumYil
+
+print(YasHesaplama(2000))
+
+#  Selamlama
+def selamlama(isim,msj= "Merhaba"):
+    return f"{msj} {isim}"
+
+print(selamlama("Mustafa"))
+
+print(selamlama("Mustafa","Hoşgeldin"))
