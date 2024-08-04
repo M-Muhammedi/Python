9c29f4decd7f318d315255f9fe0a3cc0e2b04b92
Mustafa Muhammedi <146854084+M-Muhammedi@users.noreply.github.com>
2024-08-04 16:32:09 +0300

    Add files via upload

---
+++ Listeler.py
@@ -0,0 +1,22 @@
+# Liste oluşturma
+meyveler = ["elma", "muz", "çilek"]
+print(meyveler)
+
+# Listeye öğe ekleme
+meyveler.append("portakal")
+print(meyveler)
+
+# Listeden öğe çıkarma
+meyveler.remove("muz")
+print(meyveler)
+
+# Liste öğelerine erişme
+
+print(meyveler[0])  # İlk öğe
+
+print(meyveler[-1]) # Son öğe
+
+print(meyveler[0:2]) # ilk iki öğe
+
+print(len(meyveler)) # listenin eleman sayısını gösterir
+
